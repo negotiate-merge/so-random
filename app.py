66bf191d080225da9aad707625e1b199e3eabc6a
Nigel Flintoff <nigelflintoff@hotmail.com>
2022-02-22 00:20:25 +0000

    Update the master branch with the current operational code base

--- app.py
+++ app.py
@@ -133,4 +133,6 @@ def generate():
 def usage():
     if request.method == 'GET':
         return render_template("/usage.html")
-        
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
